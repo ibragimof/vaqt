e79051a223e4ca390d7518c8975d94d2e010b4f0
Ibragimov R <w3wo@yandex.com>
2021-11-18 23:56:51 +0500

    updates

--- assets/leaflet.js
+++ assets/leaflet.js
@@ -1,26 +1,43 @@
 var mymap = L.map("map").setView(
-    [37.717835865297644, 67.60712147320088],
-    13
+    [39.65, 66.95],
+    9 
   );
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'Картография &copy; <a target="_blank" href="http://openstreetmap.org">OpenStreetMap</a>',
+    minZoom: 6,
+    maxZoom: 14,
     }).addTo(mymap);
 
   //initilaize new cordinates
   var lat;
   var lng;
+  // marker icon
+  function clickEvent(e) {
 
-  function onMapClick(e) {
-    L.marker(e.latlng)
-      .addTo(mymap)
-      .bindPopup(
-        "<b>Выбрано новое место на карте</b><br>" +
-          e.latlng.toString().slice(6)
-      )
-      .openPopup();
-      lat = e.latlng.lat;
-      lng = e.latlng.lng;
-  }
-  mymap.on("click", onMapClick);
-  
-  
+    if (typeof(newMarker)==='undefined'){
+        newMarker = new L.marker(e.latlng, { draggable: true });
+        newMarker.addTo(mymap).bindPopup(
+          "<b>Выбрано местоположение</b><br>" +
+                  e.latlng.toString().slice(6)
+              )
+              .openPopup();;}
+    else { newMarker.setLatLng(e.latlng).bindPopup(
+            "<b>Выбрано местоположение</b><br>" +
+              e.latlng.toString().slice(6)
+          )
+          .openPopup();}
+
+
+    lat = e.latlng.lat;
+    lng = e.latlng.lng;
+    console.log(lat + " Inside");
+    now();
+}
+
+mymap.on('click', clickEvent);
+console.log(lat + " Global");
+
+function now(){
+console.log(lat + " Global after click");
+}
