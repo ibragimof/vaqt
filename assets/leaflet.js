5beada32f923188e34864d33fd75dcb9e10573ca
Ibragimov R <w3wo@yandex.com>
2021-11-22 21:53:18 +0500

    Added icons for map. Draw method for found location. Still works

--- assets/leaflet.js
+++ assets/leaflet.js
@@ -1,10 +1,10 @@
-let mymap = L.map("map").setView([39.65, 66.95], 8);
+let currentMap = L.map("map").setView([39.65, 66.95], 8);
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
   attribution:
     'Imagery &copy; <a target="_blank" href="http://openstreetmap.org">OpenStreetMap</a>',
   minZoom: 3,
   maxZoom: 17,
-}).addTo(mymap);
+}).addTo(currentMap);
 
 // Custom narker icons for map:
 let logoIcon = L.icon({
@@ -15,21 +15,41 @@ let logoIcon = L.icon({
 
 let kaabahIcon = L.icon({
   iconUrl: '/assets/kaaba.png',
-  iconAnchor:   [0, 35], // point of the icon which will correspond to marker's location
-  popupAnchor:  [0, -43] // point from which the popup should open relative to the iconAnchor
+  iconAnchor:   [5, 35], // point of the icon which will correspond to marker's location
+  popupAnchor:  [18, -43] // point from which the popup should open relative to the iconAnchor
 });
 
+//initialize array for polygonal draw and getting distance:
+let markerCoordinates = Array();
 //initialize new map location:
-mymap.locate({ setView: true });
+currentMap.locate({ setView: true });
 //initilaize new users cordinates:
 let userCoordinates = new Object();
+
+// Initialize new array for Qibla distance:
+let pathToQibla = new Array();
+// And line for draving (Polyline)
+let route;
+let polygone = L.polyline;
+// Add distance variable between qibla and destination:
+let from;
+let to;
+let distance;
+
+// qibla marker initilization and save coord.s in array
+let qibla = L.marker([21.422505, 39.826216], {icon: kaabahIcon});
+qibla.addTo(currentMap).bindPopup(
+  "<b>Каъба, Макка шаҳри</b><br>(Қибла)"
+).openPopup();
+pathToQibla.push(qibla.getLatLng());
+
 
 // to detect location:
 function onLocationFound(e) {
   let radius = e.accuracy;
 
-  L.marker(e.latlng)
-    .addTo(mymap)
+  let foundLocation = L.marker(e.latlng, {icon: logoIcon})
+    .addTo(currentMap)
     .bindPopup(
       "<b>Харитадаги ўрнингиз</b><br>(Аниқлик &asymp; " +
         Math.floor(radius) +
@@ -37,11 +57,24 @@ function onLocationFound(e) {
     )
     .openPopup();
 
-  L.circle(e.latlng, radius).addTo(mymap);
+  L.circle(e.latlng, radius).addTo(currentMap);
   userCoordinates = e.latlng;
   globalScope();
+
+  // #######################
+  // Block of dirty code
+  // Draw line between Qibla and this location:
+  pathToQibla.push(foundLocation.getLatLng());
+  route = polygone(pathToQibla, {
+    color: 'red'
+  }).addTo(currentMap);
+  // get
+  from = qibla.getLatLng();
+  to = foundLocation.getLatLng();
+  distance = Math.trunc(from.distanceTo(to) / 1000);
+  document.getElementById('distance').innerHTML = distance;
 }
-mymap.on("locationfound", onLocationFound);
+currentMap.on("locationfound", onLocationFound);
 // To log error:
 function onLocationError(e) {
   alert(
@@ -49,13 +82,13 @@ function onLocationError(e) {
   );
 }
 
-mymap.on("locationerror", onLocationError);
+currentMap.on("locationerror", onLocationError);
 // marker icon
 function clickEvent(e) {
   if (typeof newMarker === "undefined") {
     newMarker = new L.marker(e.latlng);
     newMarker
-      .addTo(mymap)
+      .addTo(currentMap)
       .bindPopup(
         "<b>Жой танланди</b><br>" + e.latlng.toString().slice(6)
       )
@@ -72,6 +105,6 @@ function clickEvent(e) {
   globalScope();
 }
 
-mymap.on("click", clickEvent);
+currentMap.on("click", clickEvent);
 
 // in another file, all parameters should be used inside of globalScope() function!!!
